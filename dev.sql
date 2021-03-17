9232c7a5ceed8b3810cf8a6cadb262a54234259a
Akshay Nannaware <45255068+akshaynannaware@users.noreply.github.com>
2021-03-17 14:01:14 +0530

    Update dev.sql

--- dev.sql
+++ dev.sql
@@ -11,7 +11,6 @@ INSERT INTO studentData values('vaishnavi', 19112044, 'Dapoli, Ratnagiri', 92842
 INSERT INTO studentData values('sumit', 19111042, 'Ahmednagar', 1234567890, 'PQRT100127');
 INSERT INTO studentData VALUES('abhi', 19111120, 'kedgaon', 132122222, 'PQRT101020');
 
-INSERT INTO studentData values('zamli', 19111025, 'heart, Ratnagiri', 7972958603, 'PQRT191111');
 INSERT INTO studentData values('vicky', 19111040, 'jalgaon', 7979711979, 'PQRT101127');
 INSERT INTO studentData VALUES('onkar', 19111021, 'baramati', 1321200121, 'PQRT100120');
 INSERT INTO studentData VALUES('ankita', 19111045, 'yavatmal', 1555155514, 'PQRT100020');
